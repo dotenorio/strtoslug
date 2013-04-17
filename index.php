f9932819401bd0b6903c59bd0e499a133e2ba98d
Fernando Migliorini Tenório <fernando@agenciadeinternet.com>
2013-04-17 15:03:11 -0300

    Ajuste para melhor visualização do Github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-  	<title>StrToSlug | dotenorio.com</title>
+    <title>StrToSlug | dotenorio.com</title>
     <meta charset="utf-8">
 
     <!-- Bootstrap -->
@@ -10,83 +10,85 @@
   </head>
   <body>
 
-		<div class="container">
+    <div class="container">
 
 
-			<div class="page-header">
-				<h1>
-					StrToSlug 
-					<small>Um conversor de string para slug on-line</small>
-				</h1>
-			</div>
+      <div class="page-header">
+        <h1>
+          StrToSlug 
+          <small>Um conversor de string para slug on-line</small>
+        </h1>
+      </div>
 
-			<?php
+      <?php
 
-			require_once('strtoslug.php');
+      require_once('strtoslug.php');
 
-			if ( isset($_GET['s']) ) {
-				$string = $_GET['s'];
-			} else {
-				$string = "Digite uma frase!";
-			}
+      if ( isset($_GET['s']) ) {
+        $string = $_GET['s'];
+      } else {
+        $string = "Digite uma frase!";
+      }
 
-			$slug = new strtoslug($string);
-			$result = $slug->getResult();
+      $slug = new strtoslug($string);
+      $result = $slug->getResult();
 
-			?>
+      ?>
 
-			<div class="span12">
-				
-				<form action="./" method="get" class="form-horizontal">
-					
-					<div class="control-group">
-						<label class="control-label" for="string"><strong>String:</strong></label>
-						<div class="controls">
-							<input type="text" name="s" id="string" value="<?php echo $string; ?>" class="span7" />
-						</div>
-					</div>
+      <div class="span12">
+        
+        <form action="./" method="get" class="form-horizontal">
+          
+          <div class="control-group">
+            <label class="control-label" for="string"><strong>String:</strong></label>
+            <div class="controls">
+              <input type="text" name="s" id="string" value="<?php echo $string; ?>" class="span7" />
+            </div>
+          </div>
 
-					<div class="control-group">
-						<label class="control-label" for="slug"><strong>Slug:</strong></label>
-						<div class="controls">
-							<input type="text" id="slug" value="<?php echo $result; ?>" class="span7" />
-						</div>
-					</div>
+          <div class="control-group">
+            <label class="control-label" for="slug"><strong>Slug:</strong></label>
+            <div class="controls">
+              <input type="text" id="slug" value="<?php echo $result; ?>" class="span7" />
+            </div>
+          </div>
 
-					<div class="control-group">
-						<div class="controls">
-							<button type="submit" class="btn btn-success span4">
-								<i class="icon-white icon-ok"></i>
-								Enviar
-							</button>
-							<a href="./" class="btn btn-danger span2">
-								<i class="icon-white icon-refresh"></i>
-								Limpar
-							</a>
-						</div>
-					</div>
+          <div class="control-group">
+            <div class="controls">
+              <button type="submit" class="btn btn-success span4">
+                <i class="icon-white icon-ok"></i>
+                Enviar
+              </button>
+              <a href="./" class="btn btn-danger span2">
+                <i class="icon-white icon-refresh"></i>
+                Limpar
+              </a>
+            </div>
+          </div>
 
-				</form>
+        </form>
 
-				<div class="clearfix"></div>
-				<hr />
+        <div class="clearfix"></div>
+        <hr />
 
-				<footer class="container">
-		      <div class="pull-right">
-		        <small class="muted">
-		          desenvolvido por
-		          <a rel="license" href="http://dotenorio.com" title="Fernando Migliorini Tenório">dotenorio.com</a>
-		        </small>
-		      </div>
-		      <p class="muted">
-		        <strong>Slug</strong> parte de um URL que identifica uma página usando palavras-chave legíveis. <small class="muted">(<a href="http://en.wikipedia.org/wiki/Clean_URL" title="Clean URL - Wikipedia, the free encyclopedia" target="_blank">Wikipédia</a>)</small>
-		      </p>
-		    </footer>
+        <footer class="container">
+          <div class="pull-right">
+            <small class="muted">
+              desenvolvido por
+              <a rel="license" href="http://dotenorio.com" title="Fernando Migliorini Tenório">dotenorio.com</a>
+            </small>
+          </div>
+          <p class="muted">
+            <strong>Slug</strong> parte de um URL que identifica uma página usando palavras-chave
+            legíveis. <small class="muted">(<a href="http://en.wikipedia.org/wiki/Clean_URL" title=
+            "Clean URL - Wikipedia, the free encyclopedia" target="_blank">Wikipédia</a>)</small>
+          </p>
+        </footer>
 
-			</div>
-		</div>
+      </div>
+    </div>
 
     <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>		
-	</body>
+    <script src="js/bootstrap.min.js"></script>   
+  </body>
 </html>
--- strtoslug.php
+++ strtoslug.php
@@ -3,44 +3,47 @@
 class strtoslug
 { 
 
-	private $_result;
+  private $_result;
 
-	public function strtoslug($string)
-	{
+  public function strtoslug($string)
+  {
 
-		mb_internal_encoding('UTF-8');	
-		$string = mb_strtolower($string);
-		$string = preg_replace('/ /', '-', $string);
-		$string = preg_replace('[á|à|â|ã|ª|ä]','a',$string);
-		$string = preg_replace("[í|ì|ï]","i",$string);	
-		$string = preg_replace("[é|è|ê|ë]","e",$string);	
-		$string = preg_replace("[ó|ò|ô|õ|º|ö]","o",$string);	
-		$string = preg_replace("[ú|ù|û|ü]","u",$string);
-		$string = preg_replace("[\+|\:|\!|\@|\#|\\$|\%|\&|\*|\(|\)|\§|\¬|\¢|\£|\³|\²|\¹|\.|\,|\;|\?|\°|\<|\>|\|\\|\/|\'\"]","",$string);
-		$string = str_replace('ç', 'c', $string);
-		$string = str_replace('ñ', 'n', $string);
-		$result = $string;
-		
-		$this->setResult($result);
+    mb_internal_encoding('UTF-8');  
+    $string = mb_strtolower($string);
+    $string = preg_replace('/ /', '-', $string);
+    $string = preg_replace('[á|à|â|ã|ª|ä]','a',$string);
+    $string = preg_replace("[í|ì|ï]","i",$string);  
+    $string = preg_replace("[é|è|ê|ë]","e",$string);  
+    $string = preg_replace("[ó|ò|ô|õ|º|ö]","o",$string);  
+    $string = preg_replace("[ú|ù|û|ü]","u",$string);
+    $string = preg_replace(
+      "[\+|\:|\!|\@|\#|\\$|\%|\&|\*|\(|\)|\§|\¬|\¢|\£|\³|\²|\¹|\.|\,|\;|\?|\°|\<|\>|\|\\|\/|\'\"]","",
+      $string
+    );
+    $string = str_replace('ç', 'c', $string);
+    $string = str_replace('ñ', 'n', $string);
+    $result = $string;
+    
+    $this->setResult($result);
 
-		return $this->getResult();
+    return $this->getResult();
 
-	}
+  }
 
-	public function setResult($result)
-	{
+  public function setResult($result)
+  {
 
-		$this->_result = $result;
-		return $this->_result;
-	
-	}
-	
-	public function getResult()
-	{
+    $this->_result = $result;
+    return $this->_result;
+  
+  }
+  
+  public function getResult()
+  {
 
-		return $this->_result;
+    return $this->_result;
 
-	} 
+  } 
 
 }
 
